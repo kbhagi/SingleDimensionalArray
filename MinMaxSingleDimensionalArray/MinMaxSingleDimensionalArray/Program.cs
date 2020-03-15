d9c8c73541344bdc44a0e9cbde755ce254ddea18
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 16:50:29 +0530

    Add files via upload

---
+++ MinMaxSingleDimensionalArray/MinMaxSingleDimensionalArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMaxSingleDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 10, -10, -20, 0, 15, 20, 30 };
+            Console.WriteLine("Maximum Element: " + array1.Max());
+            Console.WriteLine("Minimum Element: " + array1.Min());
+            Console.Read();
+        }
+
+    }
+}
